624cd9fb8779ed31f63b1d73eba303cabf751dbd
AdirthaBorgohain <adirthaborgohain@gmail.com>
2019-11-22 19:58:41 +0530

    Updated results page in API

--- Tapsearch.py
+++ Tapsearch.py
@@ -10,16 +10,13 @@ class TapSearch:
     def __repr__(self):
         return str(self.inverted_index)
     
-    def extract_docs(self):
-        docs = self.text.split("\n\n")
-        return docs
-    
     def __index_doc(self, doc, doc_idx):
         if doc_idx not in self.doc_indexes:
             self.doc_indexes[doc_idx] = doc
             
         clean_doc = re.sub(r'[^\w\s]','', doc)
         clean_doc = clean_doc.lower()
+        clean_doc = clean_doc.replace("\r","")
         terms = clean_doc.split(' ')
         
         for term in terms:
@@ -33,29 +30,51 @@ class TapSearch:
                         doc_idx: { "frequency" : 1 }
                     }
                 self.inverted_index[term] = temp_dict
-    
-    def search_term(self, term):
-        term = term.lower()
-        if term in self.inverted_index:
-            print(self.inverted_index[term])
-            term_dict = self.inverted_index[term]
-            result_idx = sorted(term_dict.keys(), key=lambda x: term_dict[x]['frequency'], reverse=True)
-        for idx in result_idx:
-            print(self.doc_indexes[idx])
+                
+    def extract_docs(self):
+        docs = self.text.split("\n\n")
+        return docs
         
     def create_inverse_index(self):
         for idx, doc in enumerate(self.docs):
             self.__index_doc(doc, idx+1)
+        return self.docs
     
     def retrieve_dict(self):
         return self.inverted_index
+    
+    def retrieve_doc_dict(self):
+        return self.doc_indexes
+
+    @staticmethod
+    def search_term(inverted_index, document_index, term):
+#        print('************************')
+#        print(inverted_index)
+#        print('************************')
+#        print(document_index)
+        term = term.lower()
+        if term in inverted_index:
+            print(inverted_index[term])
+            term_dict = inverted_index[term]
+            result_idx = sorted(term_dict.keys(), key=lambda x: term_dict[x]['frequency'], reverse=True)
+            results = []
+            print(document_index)
+#            print(result_idx)
+#            for idx in result_idx:
+#                results.append(document_index[idx])
+#                print(document_index[idx])
+        else:
+            results = []
+        return results
         
 def main():
     test_obj = TapSearch('I am a dog and a very faithful VERY one.\n\nShe is very harmless.\n\nI love this college')
 #    ret = test_obj.extract_docs()
     test_obj.create_inverse_index()
 #    x = test_obj.retrieve_dict
-    test_obj.search_term('very')
+    x = test_obj.retrieve_dict()
+    y= test_obj.retrieve_doc_dict()
+    test_obj.search_term(x , y, 'very')
 #    print(x)
     
 if __name__ == "__main__":
--- app.py
+++ app.py
@@ -4,29 +4,35 @@ from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/doc', methods=['POST'])
+@app.route('/handle_document', methods=['POST'])
 def handle_document():
     doc = request.form['document']
+#    print(doc)
     TS = TapSearch(doc)
     TS.create_inverse_index()
-    session["inverted_index"] = obj.retrieve_dict()
-    session["document_index"] = retrieve_doc_dict()
-    return render_template("word.html")
+    session["inverted_index"] = TS.retrieve_dict()
+    session["document_index"] = TS.retrieve_doc_dict()
+    return render_template("function.html")
 
-@app.route('/clear', methods=['POST'])
-def clear_db():
+#@app.route('/clear', methods=['POST'])
+#def clear_db():
 
 
-@app.route('/search', methods=['POST'])
+@app.route('/search_term', methods=['POST'])
 def search_term():
     term = request.form['term']
+#    print(session["inverted_index"])
+#    print(term)
     result = TapSearch.search_term(session["inverted_index"], session["document_index"], term)
-    return render_template("result.html", paragraphs = res)
+    print('**********************')
+#    print(result)
+    return render_template("results.html", paragraphs = result)
 
 if __name__ == "__main__":
     app.run()
